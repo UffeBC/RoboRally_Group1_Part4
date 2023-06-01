42520cd58c0fdbbcc63acf3cb21961879ef55eb9
ThebigAJ <113901979+ThebigAJ@users.noreply.github.com>
2023-06-01 09:56:57 +0200

    check point and endgame

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/actions/ActionHandler.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/actions/ActionHandler.java
@@ -14,10 +14,7 @@ public class ActionHandler {
     public static void exeConveyer(String space, Player player) {
         //---------------BLUES----------------------
         // Blue Corners
-        for (int i = 0; i < 4; i++) {
-            if (space.equals(i + "C") || space.equals(i + "D"))
-                System.out.println("Blue Corner");
-        }
+
         if (space.equals("0C")) player.setHeading(player.getHeading().prev());
         if (space.equals("1C")) player.setHeading(player.getHeading().prev());
         if (space.equals("2C")) player.setHeading(player.getHeading().next());
@@ -29,20 +26,15 @@ public class ActionHandler {
         if (space.equals("3D")) player.setHeading(player.getHeading().next());
 
         // Blue Straights
-        for (int i = 4; i < 6; i++) {
-            if (space.equals(i + "B") || space.equals(i + "C"))
-                System.out.println("Blue Straight");
-        }
+
         if (space.equals("4B")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y - 2));
         if (space.equals("5B")) player.setSpace(player.board.getSpace(player.getSpace().x + 2, player.getSpace().y));
         if (space.equals("4C")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y + 2));
         if (space.equals("5C")) player.setSpace(player.board.getSpace(player.getSpace().x - 2, player.getSpace().y));
 
         // Blue Junctions
-        for (int i = 0; i < 6; i++) {
-            if (space.equals(i + "J") || space.equals(i + "K"))
-                System.out.println("Blue Junction");
-        }
+
+
         if (space.equals("0J")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y - 1));
         if (space.equals("1J")) player.setSpace(player.board.getSpace(player.getSpace().x + 1, player.getSpace().y));
         if (space.equals("2J")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y + 1));
@@ -59,10 +51,7 @@ public class ActionHandler {
 
         //-------------YELLOWS---------------------
         // Yellow Corners
-        for (int i = 0; i < 4; i++) {
-            if (space.equals(i + "E") || space.equals(i + "F"))
-                System.out.println("Yellow Corner");
-        }
+
         if (space.equals("0E")) player.setHeading(player.getHeading().prev());
         if (space.equals("0E")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y + 1));
         if (space.equals("1E")) player.setHeading(player.getHeading().prev());
@@ -81,11 +70,7 @@ public class ActionHandler {
         if (space.equals("3F")) player.setHeading(player.getHeading().next());
         if (space.equals("3F")) player.setSpace(player.board.getSpace(player.getSpace().x - 1, player.getSpace().y));
         // Yellow Straights
-        for (int i = 0; i < 4; i++) {
-            if (space.equals(i + "G")) {
-                System.out.println("Yellow Straights");
-            }
-        }
+
         if (space.equals("0G")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y - 1));
         if (space.equals("1G")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y + 1));
         if (space.equals("2G")) player.setSpace(player.board.getSpace(player.getSpace().x - 1, player.getSpace().y));
@@ -93,10 +78,7 @@ public class ActionHandler {
 
 
         // Yellow Junctions
-        for (int i = 0; i < 6; i++) {
-            if (space.equals(i + "H") || space.equals(i + "I"))
-                System.out.println("Yellow Junctions");
-        }
+
         if (space.equals("0H")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y - 1));
         if (space.equals("1H")) player.setSpace(player.board.getSpace(player.getSpace().x + 1, player.getSpace().y));
         if (space.equals("2H")) player.setSpace(player.board.getSpace(player.getSpace().x, player.getSpace().y + 1));
@@ -148,6 +130,7 @@ public class ActionHandler {
         if (space.equals("1N")){
             if(player.getCheckToken() == 0){
                 player.setCheckToken();
+
             }
         }
         else if (space.equals("2N")){
@@ -156,7 +139,7 @@ public class ActionHandler {
             }
         }
         else if (space.equals("3N")){
-            if(player.getCheckToken() == 3 ){
+            if(player.getCheckToken() == 2 ){
                 player.setCheckToken();
                 player.board.setPhase(Phase.GAME_WON);
             }
