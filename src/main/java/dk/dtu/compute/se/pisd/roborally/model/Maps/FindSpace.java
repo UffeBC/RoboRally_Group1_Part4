a1487267e5c09779202b492050c743da1d4b1ea7
tsrDTU <113907153+tsrDTU@users.noreply.github.com>
2023-06-07 08:05:00 +0200

    Dummy

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Maps/FindSpace.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Maps/FindSpace.java
@@ -7,8 +7,8 @@ public class FindSpace {
     public static String ofPlayer(Player player) {
         int x = player.getSpace().x;
         int y = player.getSpace().y;
-        //System.out.printf("playerx:%s playery:%s\r\n",x,y);
-        //System.out.println(Value.map);
+        System.out.printf("playerx:%s playery:%s\r\n",x,y);
+//        System.out.println(Value.map);
         if (Value.map == "GoldenStripe" && x>2)
             return GoldStripe.GoldStripeArray[y][x-3];
         else if (Value.map == "RingOfDeath" && x>2)
