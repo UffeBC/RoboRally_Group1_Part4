d8c01873c4db06e5247cb2322fa1981e76ede8a1
UffeBC <113894934+UffeBC@users.noreply.github.com>
2023-06-07 11:01:05 +0200

    uffe fix findspace

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/Maps/FindSpace.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/Maps/FindSpace.java
@@ -3,19 +3,21 @@ package dk.dtu.compute.se.pisd.roborally.model.Maps;
 import dk.dtu.compute.se.pisd.roborally.model.Core.Value;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
 
+import java.util.Objects;
+
 public class FindSpace {
     public static String ofPlayer(Player player) {
         int x = player.getSpace().x;
         int y = player.getSpace().y;
         //System.out.printf("playerx:%s playery:%s\r\n",x,y);
         //System.out.println(Value.map);
-        if (Value.map == "GoldenStripe" && x>2)
+        if (Objects.equals(Value.map, "GoldenStripe") && x>2)
             return GoldStripe.GoldStripeArray[y][x-3];
-        else if (Value.map == "RingOfDeath" && x>2)
+        else if (Objects.equals(Value.map, "RingOfDeath") && x>2)
             return RingOfDeath.RingOfDeath[y][x-3];
-        else if (Value.map == "WhirlWind" && x>2)
+        else if (Objects.equals(Value.map, "WhirlWind") && x>2)
             return WhirlWind.WhirlWind[y][x-3];
-        else if (Value.map == "Testing" && x>2)
+        else if (Objects.equals(Value.map, "Testing") && x>2)
             return  Testing.Testing[y][x-3];
         else return StartBoard.StartBoard[y][x];
     }
