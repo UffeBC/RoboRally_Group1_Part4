5882c798ee39fab9cfd48147ed3dc7f0693588f2
UffeBC <113894934+UffeBC@users.noreply.github.com>
2023-06-08 11:31:29 +0200

    uffe wrong url for localhost for get request

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/FileUploadController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/FileUploadController.java
@@ -7,7 +7,7 @@ import java.io.*;
 @RestController
 public class FileUploadController {
 
-    @GetMapping("/upload")
+    @PostMapping("/upload")
     public String uploadFile(/*@RequestBody String json*/) {
         try {
 
@@ -30,6 +30,41 @@ public class FileUploadController {
             ;
 //            bufferedWriter.write(json);
 //            bufferedWriter.close();
+
+            return x.toString();//"File uploaded successfully!";
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to upload the file.";
+        }
+    }
+
+
+
+
+    @GetMapping("/upload")
+    public String getFile(/*@RequestBody String json*/) {
+        try {
+
+            String json = "TestSave2";
+            // Write the JSON content to a file
+            String filePath = "target/classes/boards/TestSave.json";
+
+            FileWriter fileWriter = new FileWriter(filePath);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+
+            FileReader fileReader = new FileReader("http://10.209.211.226:8080/upload");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+
+            StringBuilder x = new StringBuilder();
+            while(bufferedReader.readLine()!=null){
+                x.append(bufferedReader.readLine());
+            }
+        //dependency injection
+
+
+            bufferedWriter.write(x.toString());
+            bufferedWriter.close();
 
             return x.toString();//"File uploaded successfully!";
         } catch (IOException e) {
