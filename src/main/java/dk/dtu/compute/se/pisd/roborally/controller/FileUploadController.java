73348b13c1c6af5466145f0251093f3d0a827ba9
UffeBC <113894934+UffeBC@users.noreply.github.com>
2023-06-09 10:53:20 +0200

    uffe wrong url for localhost for get request4

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/FileUploadController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/FileUploadController.java
@@ -1,18 +1,22 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
 import ch.qos.logback.core.model.Model;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
 import org.springframework.core.io.UrlResource;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.processing.Generated;
 import java.io.*;
 import java.net.URLConnection;
+import java.util.List;
 
 import static java.lang.System.currentTimeMillis;
 
 @RestController
 public class FileUploadController {
 
-    @PostMapping("/upload")
+    @GetMapping("/upload")
     public String uploadFile(/*@RequestBody String json*/) {
         try {
 
@@ -46,19 +50,82 @@ public class FileUploadController {
 
 
     //
-    @GetMapping(value = "/upload/{width}")
-    public String greeting(
-            @RequestParam(value="width") String width, Model model) {
-        model.getBodyText();
-        System.out.println(model.getBodyText());
-        return "hi";
+    @GetMapping(value = "/uploadt")
+    public String greeting() throws IOException {
+        String uri = "http://localhost:8080/upload";
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(uri, String.class);
+
+        String filePath = "target/classes/boards/TestSave.json";
+        FileWriter fileWriter = new FileWriter(filePath);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        assert result != null;
+        bufferedWriter.write(result);
+        bufferedWriter.close();
+        //
+        System.out.println(result);
+        //
+        return result;
     }
+    //
+
+//    //
+//    @Generated("jsonschema2pojo")
+//    public class Example {
+//
+//        public Integer width;
+//        public Integer height;
+//        public List<Object> spaces;
+//        public Integer numberOfPlayers;
+//        public List<Player> players;
+//        public String map;
+//        public Integer amountOfPlayers;
+//        public Boolean movePlayer;
+//        public Integer selectedPLayer;
+//        public Integer clickCounter;
+//
+//        /**
+//         * No args constructor for use in serialization
+//         *
+//         */
+//        public Example() {
+//        }
+//
+//        /**
+//         *
+//         * @param numberOfPlayers
+//         * @param amountOfPlayers
+//         * @param selectedPLayer
+//         * @param players
+//         * @param movePlayer
+//         * @param clickCounter
+//         * @param width
+//         * @param spaces
+//         * @param map
+//         * @param height
+//         */
+//        public Example(Integer width, Integer height, List<Object> spaces, Integer numberOfPlayers, List<Player> players, String map, Integer amountOfPlayers, Boolean movePlayer, Integer selectedPLayer, Integer clickCounter) {
+//            super();
+//            this.width = width;
+//            this.height = height;
+//            this.spaces = spaces;
+//            this.numberOfPlayers = numberOfPlayers;
+//            this.players = players;
+//            this.map = map;
+//            this.amountOfPlayers = amountOfPlayers;
+//            this.movePlayer = movePlayer;
+//            this.selectedPLayer = selectedPLayer;
+//            this.clickCounter = clickCounter;
+//        }
+//
+//    }
     //
 
     @GetMapping("/uploads")
     @ResponseBody
     public String getFile(/*@RequestBody String json*/) {
         try {
+
 
             //String json = "TestSave2";
             // Write the JSON content to a file
@@ -68,6 +135,8 @@ public class FileUploadController {
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
             UrlResource resource = new UrlResource("http://localhost:8080/update");
+
+
 
 //            @RequestParam
            // @RequestBody String s
@@ -92,6 +161,8 @@ public class FileUploadController {
             bufferedWriter.close();
 
             return String.valueOf(UrlResource.from("http://localhost:8080/update").contentLength());//"File uploaded successfully!";
+            // 10.209.211.226
+
         } catch (IOException e) {
             e.printStackTrace();
             return "Failed to upload the file.";
